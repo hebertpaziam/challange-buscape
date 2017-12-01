464e175a0e487a6b9ee30b9a3fad2cafb09e2bd5
hebert marcos pazian <hebertpazian@hotmail.com>
2017-12-01 20:32:09 -0200

    gulp implemented

---
+++ gulpfile.js
@@ -0,0 +1,44 @@
+'use strict';
+
+const gulp = require('gulp');
+const gulpif = require('gulp-if');
+const sass = require('gulp-sass');
+const strip = require('gulp-strip-css-comments');
+const autoprefixer = require('gulp-autoprefixer');
+const sourcemaps = require('gulp-sourcemaps');
+const minify = require('gulp-minifier');
+
+var isProduction = false;
+
+/* ===================== STYLES =====================*/
+gulp.task('styles', function () {
+    gulp.src('./source/styles/*.scss')
+        .pipe(sass())
+        .pipe(autoprefixer({ browsers: ['last 2 versions'] }))
+        .pipe(strip())
+        .pipe(gulpif(isProduction, minify({
+            minify: true,
+            minifyCSS: true,
+            collapseWhitespace: true,
+            conservativeCollapse: true,
+            getKeptComment: function (content, filePath) {
+                var m = content.match(/\/\*![\s\S]*?\*\//img);
+                return m && m.join('\n') + '\n' || '';
+            }
+        })))
+        .pipe(gulp.dest('./public/styles'))
+});
+
+
+/*/////////////////////////////////////////////////////
+                         UTILS
+/////////////////////////////////////////////////////*/
+
+gulp.task("makeBuildCompilation", function () { isProduction = true; })
+gulp.task("build", ["makeBuildCompilation", 'styles'])
+
+gulp.task('watch', function () {
+    gulp.watch(paths.srcFiles.styles, ['styles']);
+});
+gulp.task("dev", ['styles', 'watch']);
+gulp.task('default', ['styles']);
