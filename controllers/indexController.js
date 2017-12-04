7a798a63ad32b5d4e61ae12431ff59796cee9da3
hebert marcos pazian <hebertpazian@hotmail.com>
2017-12-04 12:31:49 -0200

    calculando subtotal server side

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -2,18 +2,31 @@ const Product = require('../models/productModel');
 const db = require('../database/mongoose');
 
 exports.renderIndex = (req, res) => {
-    var list
-    var cart
+    var list;
+    var cart;
+    var subtotal = {
+        installmentValue: 0,
+        value: 0
+    };
+
     Product.find().then((products) => {
         list = products;
     }).then(() => {
         Product.find({
             cart: true
         }).then((products) => {
+
+            products.forEach((prod)=>{
+                subtotal.installmentValue += prod.price.installmentValue
+                subtotal.value += prod.price.value
+            })
+
             cart = products;
+
             res.render('index', {
                 list: list,
-                cart: cart
+                cart: cart,
+                subtotal:subtotal
             });
         })
     });
